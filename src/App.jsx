4df43dbc9a9b24f8435864b398e58595872c237a
h662 <h662hong@gmail.com>
2023-11-09 20:57:01 +0900

    Get Coin Price

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const App = () => {
+  const getCoinPrice = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.upbit.com/v1/ticker?markets=KRW-BTC,%20KRW-ETH,%20KRW-MATIC"
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCoinPrice();
+  }, []);
+
   return <div className="bg-red-100">Hello, React!</div>;
 };
 
