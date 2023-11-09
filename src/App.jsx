86c7ce7a134525b96147fa4e14f0c2cf05aec649
h662 <h662hong@gmail.com>
2023-11-09 21:14:12 +0900

    Coin listing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [coinPrices, setCoinPrices] = useState();
+
   const getCoinPrice = async () => {
     try {
       const response = await axios.get(
         "https://api.upbit.com/v1/ticker?markets=KRW-BTC,%20KRW-ETH,%20KRW-MATIC"
       );
 
-      console.log(response);
+      setCoinPrices(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -18,7 +20,29 @@ const App = () => {
     getCoinPrice();
   }, []);
 
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <ul className="flex flex-col gap-8">
+        {coinPrices ? (
+          coinPrices.map((v, i) => {
+            return (
+              <li key={i} className="flex items-center">
+                <img
+                  className="w-6 inline-block"
+                  src={`./images/${v.market.substring(4)}.png`}
+                  alt={v.market.substring(4)}
+                />
+                <span className="ml-2">{v.market.substring(4)}</span>
+                <span className="ml-2">{v.trade_price}</span>
+              </li>
+            );
+          })
+        ) : (
+          <div>로딩중...</div>
+        )}
+      </ul>
+    </main>
+  );
 };
 
 export default App;
